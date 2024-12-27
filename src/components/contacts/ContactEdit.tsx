2e3116fcb5f99ddf655fb6d30cc64ddf434846ed
Hubbitron <hubbitron@gmail.com>
2024-12-27 10:27:55 -0500

    Added bootstrap for button, corrected navigation for post update, added logic to handle updated

--- src/components/contacts/ContactEdit.tsx
+++ src/components/contacts/ContactEdit.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router'
+import { NavigateFunction, useNavigate, useParams } from 'react-router'
 import { callFetch } from '../helper/Global';
 import { Contact } from './model/Contact';
 import { useForm } from 'react-hook-form';
+import { Button } from 'react-bootstrap';
 
 const ContactEdit = () => {
   
   let {paramId} = useParams();
   paramId = !paramId ? "" : paramId;
   
+  let navigate: NavigateFunction = useNavigate();
+
   const [id] = useState(paramId);
   const form = useForm<Contact>({
     defaultValues: async (): Promise<Contact> => {
@@ -24,12 +27,30 @@ const ContactEdit = () => {
   const { register, handleSubmit, formState } = form;
   const { errors, isValid } = formState;
   
+  const onSubmit = async (formData: Contact): Promise<void> => {
+    const contact = new Contact();
+    contact.id = formData.id;
+    contact.lastName = formData.lastName;
+    contact.firstName = formData.firstName;
+    console.log("contact = ", contact);
+    const response = await callFetch("http://localhost:8080/ContactsApi/api/update", "PUT", JSON.stringify(contact));
+    if (!response.ok) {
+      alert("Update failed");
+      return;
+    }
+    
+    navigate("/");
+  }
+
+  
+
+  
   return (
     <div className = "App">
       <h2 className='page header'>
         Contact Edit Page
       </h2>
-      <form name="ContactEdit" id="ContactEdit">
+      <form name="ContactEdit" id="ContactEdit" onSubmit={handleSubmit(onSubmit)}>
         <table>
           <tbody>
             <tr>
@@ -70,6 +91,9 @@ const ContactEdit = () => {
             </tr>
           </tbody>
         </table>
+        <Button variant = "secondary" type = "submit">
+          Submit
+        </Button>
       </form>
     </div>
   )
--- src/components/helper/Global.ts
+++ src/components/helper/Global.ts
@@ -1,7 +1,7 @@
-import React from 'react'
 
 export const callFetch = (endpoint: string, method: string, json: string) => {
     let myHeadersNoAuth = {
+
         "Content-Type" : "application/json",
         "Accept" : "application/json",
 
@@ -9,6 +9,16 @@ export const callFetch = (endpoint: string, method: string, json: string) => {
         'Access-Control-Allow-Origin': '*',
     };
     
+
+
     let myInit = {method: method, headers: myHeadersNoAuth};
+
+    let myInitWithBody = {method: method, headers: myHeadersNoAuth, body: json};
+
+    if (json) {
+        return fetch (endpoint, myInitWithBody);
+    }
+
     return (fetch (endpoint, myInit));
+    
 };
